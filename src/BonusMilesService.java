8296e0e4ad03c29180bebb2b3733b022441f13e9
Liliya_S <lilishadlam@gmail.com>
2023-10-18 23:34:20 +0700

    Second task

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int cost = 20;
+        int bonus = price / cost;
+        return bonus;
+
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int price = 20;
-        int cost = 13676;
-        int mile = cost / price;
-        System.out.println("Количество бонусных миль " + mile);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
